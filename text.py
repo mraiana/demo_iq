7424705799272e9ce82deb8c95943de67c11e4cb
melissa <116475962+mraiana@users.noreply.github.com>
2024-02-24 11:07:09 +0600

    Update text.py

--- text.py
+++ text.py
@@ -32,3 +32,28 @@ txt_res.append('''
 выше среднего''')
 txt_res.append('''
 высокая''')
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+questions = [
+    Question("2x+65=77, x=?\n(a) 4\n(b) 6\n(c) 5\n", "b"),
+    Question("Если самолет упадет на границе США и Канады, где будут похоронены выжившие?\n(a) США\n(b) Канада\n(c) Ни то, ни другое\n", "c"),
+    Question("У отца Мэри есть 5 дочерей: Лейла, Николь, Оливия, Памела. Как зовут пятую дочь?\n(a) Ребекка\n(b) Мэри\n(c) Селена\n", "b"),
+    Question("Какое число лучше всего завершает аналогию: 8:4 как 10:?\n(a) 5\n(b) 3\n(c) 2\n", "a"),
+    Question("Какое число должно быть следующим в ряду? 1, 1, 2, 3, 5, 8, 13...\n(a) 28\n(b) 21\n(c) 26\n", "b"),
+    Question("Если умножить меня на 4, а затем вычесть 5, получится 47. Какое я число?\n(a) 14\n(b) 17\n(c) 13\n", "c")
+]
+
+def run_test(questions, age):
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 3
+            global iq
+            iq = (score*6/age)*100
+    print("IQ результат:", iq)
+
+run_test(questions)
